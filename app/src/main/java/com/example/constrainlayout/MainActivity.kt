a918d7c9b5b22770998f9fde1e32618ef07b4f35
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-09-29 13:14:35 +0200

    Creado controlador del switch

--- app/src/main/java/com/example/constrainlayout/MainActivity.kt
+++ app/src/main/java/com/example/constrainlayout/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.constrainlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switch : Switch = findViewById(R.id.switch2)
+        val imagen : ImageView = findViewById(R.id.imageView)
+
+        var bol = true
+        switch.setOnClickListener { view ->
+            if (bol) {
+                imagen.setImageDrawable(getDrawable(R.drawable.messi))
+                bol = false
+            } else {
+                imagen.setImageDrawable(null)
+                bol = true
+            }
+        }
     }
 }
